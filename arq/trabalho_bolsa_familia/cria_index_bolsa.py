455a36ed493491922edba13a812ff950066b13eb
ConradoLuiz <conradol.vargas@gmail.com>
2019-12-08 16:19:14 -0300

    trabalho pronto

--- arq/trabalho_bolsa_familia/cria_index_bolsa.py
+++ arq/trabalho_bolsa_familia/cria_index_bolsa.py
@@ -1,23 +1,24 @@
 import os
 import pickle
+from bptree import BPlusTree
 
-arquivo_indexar = "bolsa10.csv"
-meu_hash = {}
+# LEIA O README
+
+arquivo_indexar = "bolsa.csv"
+tree = BPlusTree(order=300)
 with open(arquivo_indexar, "r") as f:
 	lenght = f.seek(0,2)
 	f.seek(0,0)
 	line = f.readline() # ler a header do csv
-	
 	while line:
 		num_linha = f.tell()
 		line = f.readline()
 		campos = line.split("\t")
 		if len(campos) > 1:
-			meu_hash[campos[7]] = num_linha
-		percent = ((num_linha/lenght) * 100)
-		print(f"{percent:.2f}% lido")
+			tree.insert(campos[7], num_linha)
+		percent = round(((num_linha/lenght) * 100), 5)
+		if percent % 0.2 == 0:
+			print(f"{percent}% lido")
 		
-
-
-with open("meu-hash.pickle", "wb") as f_pickle:
-	pickle.dump(meu_hash, f_pickle)
+with open("bpt_index.pickle", "wb") as f_pickle:
+	pickle.dump(tree, f_pickle)
--- arq/trabalho_bolsa_familia/le_index.py
+++ arq/trabalho_bolsa_familia/le_index.py
@@ -1,13 +1,22 @@
 import pickle
+from bptree import BPlusTree
 
-with open("meu-hash.pickle", "rb") as f_pickle:
-	meu_hash = pickle.load(f_pickle)
+# LEIA O README
+
+with open("bpt_index.pickle", "rb") as f_pickle:
+	print("Carregando index...")
+	tree = pickle.load(f_pickle)
+	print("Index carregado!")
 
 
-with open("bolsa10 - 2.csv", "r")  as f_in, open("bolsa4.csv", "w") as f_out:
+with open("BolsaFamilia_Pagamentos.csv", "r")  as f_in, open("intersecao_bolsa.csv", "w") as f_out:
 	header = f_in.readline()
 	f_out.write(header)
+	i = 0
 	for line in f_in:
-		campos = line.split("\t")
-		if campos[7] in meu_hash:
+		campos = line.split(";")
+		campos = list(map(lambda x: x.strip('"'), campos))
+		if tree.retrieve("000"+campos[5]):
 			f_out.write(line)
+			i += 1
+			print(f"Total escritos no novo arquivo: {i}")
